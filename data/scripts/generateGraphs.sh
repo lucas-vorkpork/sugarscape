6306098a6ed10b6d19826a142504f502999b7ba3
Willem Hueffed <whueffed@seattleu.edu>
2023-08-02 16:22:10 -0700

    random stuff

--- data/scripts/generateGraphs.sh
+++ data/scripts/generateGraphs.sh
@@ -1,15 +1,26 @@
 models=( benthamHalfLookaheadBinary benthamHalfLookaheadTop benthamNoLookaheadTop egoisticHalfLookaheadTop rawSugarscape )
 graphs=( meanTimeToLive percentPopGrowth starvationDeaths wealthCollected totalWealth)
-scripts=( scrapeMeanTimeToLive scrapePercentPopGrowth scrapeStarvationDeaths scrapeWealthCollected scrapeTotalWealth)
-scrapedDataPath=$(mktemp -d tempDataDir.XXX)
-cd $scrapedDataPath
+scrapingScripts=( scrapeMeanTimeToLive scrapePercentPopGrowth scrapeStarvationDeaths scrapeWealthCollected scrapeTotalWealth)
+plottingScripts=( )
+tempDir=$(mktemp -d tempDataDir.XXX)
+cd $tempDir
+absDir=$(pwd)
+for model in "${models[@]}"
+do
+    mkdir $model
+    cd $model
+    for ((index=0; index<${#graphs[@]}; index++))
+    do
+        python3 "../../"${scrapingScripts[$index]}.py -p "../../../../jsonTest" -l ${graphs[$index]}".dat" -m $model
+    done
+    cd ..
+done
+cd ../../../plots
 
 for model in "${models[@]}"
 do
-    for ((index=0; index<${#graphs[@]}; index++))
-    do
-        python3 "../"${scripts[$index]}.py -p "../../../jsonTest" -l ${graphs[$index]}.".dat" -m $model
-    done
+    for 
 done
+
 
 
---
+++ data/scripts/plotStarvationDeaths.gp
@@ -0,0 +1,5 @@
+set title "Starvation Deaths"
+set xlabel "Timestep"
+set ylabel "Number of Starvation Deaths"
+set lt 1 lw 2 lc "black"
+plot 'sppg.dat' with linespoints lt 1 dt 1 pt 0
